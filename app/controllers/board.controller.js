cd5cd7686f2b610bd55ccc1a357834dd42c310de
joenjonghyun <jjh980814@naver.com>
2022-04-14 19:17:15 +0900

    22.4.14

--- app/controllers/board.controller.js
+++ app/controllers/board.controller.js
@@ -1,17 +1,17 @@
 const db = require('../models/index')
 const BoardSchema = db.board
-exports.boardform = (req, res)=>{
+exports.write = (req, res)=>{
     new BoardSchema(req.body).save(()=>{
         res.status(200).json({'result':'ok'}) 
     })
 }
 
 exports.boardlist = (req, res)=>{
-    console.log(`### boardController access`)
+    console.log(`### BoardController access`)
     BoardSchema.find()
-    .exec((err, board) => {
+    .exec((err, Boards) => {
         if (err) return res.status(400).send(err)
-        res.status(200).json({ success: true, board})
+        res.status(200).json({ success: true, Boards})
     })
 
 }
--- app/routes/board.route.js
+++ app/routes/board.route.js
@@ -1,5 +1,5 @@
-const { boardform, boardlist } = require('../controllers/board.controller');
+const { write, boardlist } = require('../controllers/board.controller');
 module.exports = x => {
-    x.app.post(`${x.url}/write`, boardform)
+    x.app.post(`${x.url}/write`, write)
     x.app.get(`${x.url}/list`, boardlist)
 }
